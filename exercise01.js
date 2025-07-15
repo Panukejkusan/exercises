86936dca4792f8e6a77f7a92d294f77bd237b150
Karol Rucinski <k.d.rucinski@gmail.com>
2025-07-15 21:19:28 +0200

    second commit

---
+++ exercise01.js
@@ -0,0 +1,11 @@
+//Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+function boolToWord(bool){
+  if(bool == 1){
+    return "Yes";
+  } else {
+    return "No";
+  }
+}
+
+boolToWord(false);
